e80f0653e2c16e791ba7bbe48c28ef349debfce3
Mark C. Kuzyk <mark@Marks-MacBook-Pro.local>
2019-01-16 21:07:23 -0800

    restructured prologix class with instrument

--- prologix.py
+++ prologix.py
@@ -3,36 +3,50 @@ import time
 
 class prologix(object):
 
-	def __init__(self, gpib, port, baud=9600, timeout=3):
-		self.inst = serial.Serial(port, baudrate=baud, timeout=timeout)
-		if self.inst.in_waiting:
-			_junk = self.inst.read(self.inst.in_waiting)
-		self.inst.write('++mode 1\r'.encode('utf-8'))
-		self.inst.flush()
-		self.inst.write('++auto 0\r'.encode('utf-8'))
-		self.inst.flush()
-		self.inst.write('++addr {}\r'.format(gpib).encode('utf-8'))
+    def __init__(self, port, baud=9600, timeout=3):
+        self.inst = serial.Serial(port, baudrate=baud, timeout=timeout)
+        if self.inst.in_waiting:
+            _junk = self.inst.read(self.inst.in_waiting)
+        self.inst.write('++mode 1\r'.encode('utf-8'))
+        self.inst.flush()
+        self.inst.write('++auto 0\r'.encode('utf-8'))
+        self.inst.flush()
+
+    def open_instrument(self, gpib_address):
+        return instrument(gpib_address, self.inst)
+
+class instrument(object):
+
+    def __init__(self, gpib, serial_object):
+        self.inst = serial_object
+        self.gpib = gpib
+
+    def set_address(self):
+        self.inst.write('++addr {}\r'.format(self.gpib).encode('utf-8'))
+        self.inst.flush()
 
 
-	def write(self, asciiMessage):
-		asciiMessage += '\r'
-		message = asciiMessage.encode('utf-8')
-		self.inst.write(message)
-		self.inst.flush()
+    def write(self, asciiMessage):
+        self.set_address()
+        asciiMessage += '\r'
+        message = asciiMessage.encode('utf-8')
+        self.inst.write(message)
+        self.inst.flush()
 
-	def read(self):
-		response = bytes()
-		self.inst.write('++read\r'.encode('utf-8'))
-		self.inst.flush()
-		time.sleep(.1)
-		while self.inst.in_waiting:
-			response += self.inst.read(ser.in_waiting)
-			time.sleep(.1)
-		return response
+    def read(self):
+        self.set_address()
+        response = bytes()
+        self.inst.write('++read\r'.encode('utf-8'))
+        self.inst.flush()
+        time.sleep(.1)
+        while self.inst.in_waiting:
+            response += self.inst.read(self.inst.in_waiting)
+            time.sleep(.1)
+        return response
 
-	def query(self, asciiMessage):
-		self.write(asciiMessage)
-		response = self.read()
-		return response
+    def query(self, asciiMessage):
+        self.write(asciiMessage)
+        response = self.read()
+        return response
 
 
